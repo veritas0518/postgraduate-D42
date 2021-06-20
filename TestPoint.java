146d5b4fff4f4d913ebd9d280b072aba1211bf65
TianXing Xue <76930817+veritas0518@users.noreply.github.com>
2021-06-20 18:45:33 +0800

    Deriving point3d from the point2d class
    【问题描述】已知二维空间点Point2D类的定义如下，在此基础上派生出三维空间点Point3D类。根据数学知识，增加成员变量z，定义三个参数的构造方法和无参数的构造方法（对应原点的对象），重写toString()和getDistance()方法，重写equals()方法。
    在TestPoint类的main()方法中定义2个Point2D点对象，其中p21的横纵坐标从键盘输入，p22的横纵坐标为默认值；再定义2个Point3D点对象，其中p31的横纵坐标从键盘输入，p32的横纵坐标为默认值。
    分别求p21和p31到原点的距离，判断p21和p22是否重合，p31和p32是否重合，判断所有的点对象是否为原点。输入输出格式如样例所示，其中：红色文字为真正的程序输入，蓝色文字为输入提示。
    
    public class Point2D {
        protected double x, y;
        public Point2D(double x, double y) {
            this.x = x;
            this.y = y;
        }
        public Point2D() {
            this(0, 0);
        }
        //返回坐标形式的字符串
        public String toString() {
            return "(" + x + ", " + y + ")";
        }
        //判断两个二维点是否重合
        public boolean equals(Point2D p) {
            return x == p.x && y == p.y;
        }
        //求到原点距离
        public double getDistance() {
            return Math.sqrt(x * x + y * y);
        }
        //判断是否是原点
        public boolean isZero() {
            return getDistance() == 0;
        }
    }
    【样例输入1】
    3 4
    3 4 5
    
    【样例输出1】
    Please enter the coordinates of p21:
    3 4
    The distance from (3.0, 4.0) to the origin is 5.0
    Is (3.0, 4.0) the origin? -- false
    Is (0.0, 0.0) the origin? -- true
    Does (3.0, 4.0) coincide with (0.0, 0.0)? -- false
    Please enter the coordinates of p31:
    3 4 5
    The distance from (3.0, 4.0, 5.0) to the origin is 7.0710678118654755
    Is (3.0, 4.0, 5.0) the origin? -- false
    Is (0.0, 0.0, 0.0) the origin? -- true
    Does (3.0, 4.0, 5.0) coincide with (0.0, 0.0, 0.0)? -- false
    
    【样例输入2】
    2 6
    0 0 0
    
    【样例输出2】
    Please enter the coordinates of p21:
    2 6
    The distance from (2.0, 6.0) to the origin is 6.324555320336759
    Is (2.0, 6.0) the origin? -- false
    Is (0.0, 0.0) the origin? -- true
    Does (2.0, 6.0) coincide with (0.0, 0.0)? -- false
    Please enter the coordinates of p31:
    0 0 0
    The distance from (0.0, 0.0, 0.0) to the origin is 0.0
    Is (0.0, 0.0, 0.0) the origin? -- true
    Is (0.0, 0.0, 0.0) the origin? -- true
    Does (0.0, 0.0, 0.0) coincide with (0.0, 0.0, 0.0)? -- true

---
+++ TestPoint.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+public class TestPoint {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter the coordinates of p21:");
+        double x = s.nextDouble();
+        double y = s.nextDouble();
+        Point2D p21 = new Point2D(x,y);
+        Point2D p22 = new Point2D();
+        System.out.println("* p21 *");
+        System.out.println("* "+p21.getDistance());
+        System.out.println("* "+p21.isZero());
+        System.out.println("* "+p22.isZero());
+        System.out.println("* "+p21.equals(p22));
+        System.out.println("Please enter the coordinates of p31:");
+        x=s.nextDouble();
+        y=s.nextDouble();
+        double z =s.nextDouble();
+        Point3D p31 =new Point3D(x,y,z);
+        Point3D p32 =new Point3D();
+        System.out.println("* p31 *");
+        System.out.println("* "+p31.getDistance());
+        System.out.println("* "+p31.isZero());
+        System.out.println("* "+p31.isZero());
+        System.out.println("* "+p31.equals(p32));
+
+
+
+    }
+}
+class Point2D{
+        protected double x, y;
+        public Point2D(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+        public Point2D() {
+            this(0, 0);
+        }
+        //返回坐标形式的字符串
+        public String toString() {
+            return "(" + x + ", " + y + ")";
+        }
+        //判断两个二维点是否重合
+        public boolean equals(Point2D p) {
+            return x == p.x && y == p.y;
+        }
+        //求到原点距离
+        public double getDistance() {
+            return Math.sqrt(x * x + y * y);
+        }
+        //判断是否是原点
+        public boolean isZero() {
+            return getDistance() == 0;
+        }
+}
+class Point3D extends Point2D{
+    protected double z;
+    public Point3D(double x, double y,double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public Point3D() {
+        this(0, 0, 0);
+    }
+
+    public String toString() {
+        return "(" + x + ", " + y + "," + z + ")";
+    }
+
+    public boolean equals(Point3D p) {
+        return x == p.x && y == p.y && z == p.z;
+    }
+
+    public double getDistance() {
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+
+    public boolean isZero() {
+        return getDistance() == 0;
+    }
+
+}
